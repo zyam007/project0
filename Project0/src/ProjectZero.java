b8951b6e001f87d5af5cb0fda1f753c08e57e51c
Scott Dexter <sdexter@brooklyn.cuny.edu>
2015-09-04 09:44:22 -0400

    Update skeleton source.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class ProjectZero {
 
 	public static void main(String[] args) {
-		Scanner inputSource = new Scanner(System.in);
 		
-		inputSource.close();
+			Scanner inputSource = new Scanner(System.in);
+			int input1, input2, result;
+			
+			System.out.print("Please enter two numbers of your exalted choosing: ");
+			input1 = inputSource.nextInt();
+			input2 = inputSource.nextInt();
+			
+			result = input1 + input2;
+			System.out.println(result);
+			
+			inputSource.close();
+			
 	}
+
 }
+
